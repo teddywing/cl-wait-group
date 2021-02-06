dc5f0fd9a8501b5c3a611f84c452aa858c2ad29a
Teddy Wing <git@teddywing.com>
2021-02-06 18:27:33 +0100

    Add documentation tasks

--- src/wait-group.lisp
+++ src/wait-group.lisp
@@ -1,27 +1,38 @@
+;;;; TODO
+
+;; TODO: Add a wg:clear
+
 (in-package :wait-group)
 
 (defclass wait-group ()
   ((counter
      :initform 0
-     :accessor counter)))
+     :accessor counter
+     :documentation "TODO"))
+
+  (:documentation "TODO"))
 
 (defun make-wait-group ()
+  "TODO"
   (make-instance 'wait-group))
 
 (defgeneric add (wait-group &optional amount)
-  (:documentation ""))
+  (:documentation "TODO"))
 
 (defmethod add ((wait-group wait-group) &optional (amount 1))
+  "TODO"
   (incf (counter wait-group) amount))
 
 (defgeneric done (wait-group)
-  (:documentation ""))
+  (:documentation "TODO"))
 
 (defmethod done ((wait-group wait-group))
+  "TODO"
   (decf (counter wait-group)))
 
 (defgeneric wait (wait-group)
-  (:documentation ""))
+  (:documentation "TODO"))
 
 (defmethod wait ((wait-group wait-group))
+  "TODO"
   (loop until (= (counter wait-group) 0)))
--- wait-group.asd
+++ wait-group.asd
@@ -1,5 +1,7 @@
 (asdf:defsystem wait-group
+  :description "TODO"
   :version "0.0.1"
+
   :components ((:module "src"
                 :serial t
                 :components ((:file "package")
